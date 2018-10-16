5228da51d085de9f9e23a037e313d4c555bbbd9c
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-16 19:04:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 // Write your code in this file!
 
 function scuberGreetingForFeet(distance) {
-  if (distance =< 400) {
+  if (distance < 400) {
     return 'Something';
-  }
-  
-  
-  else if (distance > 2500) {
+  } else if (distance > 2500) {
     return 'No can do.';
     }
     
