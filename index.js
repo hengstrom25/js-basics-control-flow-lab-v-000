a153f89b1913468980a4841a616f7518982014a8
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-16 19:59:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
       return 'Thank you so much.';
+      break;
     case 'not as generous':
       return 'Thank you';
+      break;
   }
 }
