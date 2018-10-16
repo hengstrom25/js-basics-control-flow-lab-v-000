299e67b96a961f1d4f1a7a47365cdf51b408e3ad
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-16 19:57:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
-      return 'Thank you so much'
+      return 'Thank you so much.'
   }
 }
