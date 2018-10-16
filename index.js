7cf9d91b3799731fb562ba99e97ab781bbca1701
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-16 19:11:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return 'This one is on me!';
   } else if (distance > 2000) {
-    return 'I will gladly take your thirty bucks';
+    return 'I will gladly take your thirty bucks.';
   }else if (distance > 2500) {
     return 'No can do.';
     }
